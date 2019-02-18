9255b56c1cf09c8311f23974190f019554f2a121
Tony Cheung <iamtonycheung@gmail.com>
2019-02-19 00:59:45 +0800

    after lesson 163

--- advcbv/advcbv/urls.py
+++ advcbv/advcbv/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from basic_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.IndexView.as_view()),
+    path('basic_app/',include('basic_app.urls',namespace='basic_app'))
 ]
